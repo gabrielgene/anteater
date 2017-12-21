ce10f315572f37644034354149ca6149fd2de890
ravi <rafael.vitor@jusbrasil.com.br>
2017-12-21 12:16:52 -0300

    Rename SimpleMap to Map

--- src/components/map/index.jsx
+++ src/components/map/index.jsx
@@ -3,7 +3,7 @@ import GoogleMapReact from 'google-map-react';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-class SimpleMap extends Component {
+class Map extends Component {
   static defaultProps = {
     center: {lat: 59.95, lng: 30.33},
     zoom: 11
