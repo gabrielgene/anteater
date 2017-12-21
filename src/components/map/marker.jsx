fca84a08cf118ca6dd57d8ccdbc07bcad1f999a2
ravi <rafael.vitor@jusbrasil.com.br>
2017-12-21 13:57:41 -0300

    Add map marker component

---
+++ src/components/map/marker.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Icon } from 'semantic-ui-react';
+
+const MapMarker = () => (
+  <Icon className="MapMarker" name="pin" size="huge" />
+);
+
+export default MapMarker;
