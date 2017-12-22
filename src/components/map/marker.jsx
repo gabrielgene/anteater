4b6c43b80c4b0a46ca6e1a902982db79f9a75bde
ravi <rafael.vitor@jusbrasil.com.br>
2017-12-22 00:20:35 -0300

    Add different itens to markers depending on weather

--- src/components/map/marker.jsx
+++ src/components/map/marker.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import { Icon } from 'semantic-ui-react';
 
 const MapMarker = ({ weather }) => (
-  <Icon className="MapMarker" color={weather === 'sun' ? 'yellow' : 'blue'} name="pin" size="huge" />
+  <Icon
+    className="MapMarker"
+    color={weather === 'sun' ? 'yellow' : 'blue'}
+    name={weather === 'sun' ? 'sun' : 'rain'}
+    size="huge"
+  />
 );
 
 export default MapMarker;
